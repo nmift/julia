5a4792008318ea6297754108b5bc9dad810dcdf6
nmift <46844183+nmift@users.noreply.github.com>
2020-12-28 12:22:34 +0300

    Create zadacha_4.jl

---
+++ zadacha_4.jl
@@ -0,0 +1,27 @@
+include("../roblib.jl")
+function mark_and_enumerate!(r, side::HorizonSide)
+    steps = 0
+    
+    putmarker!(r)
+    while isborder(r, side) == false 
+        move!(r, side)
+        putmarker!(r) 
+        steps += 1
+    end
+    
+    return steps 
+end
+function mark_ladder(r)
+    num_steps = through_rectangles_into_angle(r, (Down, Left))
+
+    cells_to_mark = mark_and_enumerate!(r, Right) - 1
+    while isborder(r, Up) == false
+        movements!(r, Left)
+        move!(r, Up)
+        putmarkers!(r, Right, cells_to_mark)
+        cells_to_mark -= 1
+    end
+
+    through_rectangles_into_angle(r, (Down, Left))
+    movements!(r, (Up, Right), reverse(num_steps))
+end
