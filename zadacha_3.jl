1c33ee64c8263dc93aba4ffe911e74cbbcfa59d2
nmift <46844183+nmift@users.noreply.github.com>
2020-12-28 12:19:30 +0300

    Create zadacha_3.jl

---
+++ zadacha_3.jl
@@ -0,0 +1,17 @@
+include("../roblib.jl")
+
+function mark_all_cells(r)
+    num_steps = through_rectangles_into_angle(r, (Down, Left))
+
+    side = Right
+    while (isborder(r, Up) == false) || (isborder(r, Right) == false)
+        putmarkers!(r, side)
+        if (isborder(r, Up) == false)
+            move!(r, Up)
+        end
+        side = inverse(side)
+    end
+
+    through_rectangles_into_angle(r, (Down, Left))
+    movements!(r, (Up, Right), reverse(num_steps))
+end
